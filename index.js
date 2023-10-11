00b93cee5010a557dcc38a8ed5386487d5adfa2f
nekmichalakis <nekmichalakis@gmail.com>
2023-10-11 15:02:24 +0300

    more mongoose

--- index.js
+++ index.js
@@ -12,72 +12,81 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
     }
-]
+
+    next(error)
+}
+
+//this has to be the last loaded middleware
+app.use(errorHandler)
 
 app.get('/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date().toString()}</p>`
-    )
+    Person.countDocuments({})
+        .then(count => 
+            response.send(
+                `<p>Phonebook has info for ${count} people</p>
+                <p>${new Date().toString()}</p>`
+            )
+        )
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(people => {
+            response.json(people)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => id === p.id)
-
-    person
-    ?
-    response.json(person)
-    :
-    response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            person
+            ?
+            response.json(person)
+            :
+            response.status(404).end()
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-
+app.post('/api/persons', (request, response, next) => {
     const person = new Person({
-        name: body.name,
-        number: body.number    
+        name: request.body.name,
+        number: request.body.number    
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
-    
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
